8203ee11f09b3ffbba333ed0d6cf29d3db1b9314
ZiFeng1111 <zfwong1111@gmail.com>
2021-06-16 10:10:38 +0800

    upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var a=prompt("Enter your Name");
+var b=prompt("Enter your date of birth (format : DDMMYYYY)");
+  	if(a== "chia pik ying" && b=="20071999")
+  	{
+  	    window.location.href="2.html";
+  	}
+  	else
+  	{
+  		alert("your name should be chia pik ying\nyour date of birth should be 20071999");
+  		window.location.href="open_this_file.html";
+  	}
+  	
