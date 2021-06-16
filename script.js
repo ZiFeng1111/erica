c7c12ebf4993c470ef37ef25a7cbbd0578a9d3a1
ZiFeng1111 <84433060+ZiFeng1111@users.noreply.github.com>
2021-06-16 18:24:38 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 var a=prompt("Enter your Name (format : all small letters)");
 var b=prompt("Enter your date of birth (format : DDMMYYYY)");
-  	if(a== "wong jian hao" && b=="08032002")
+  	if(a== "chia pik ying" && b=="12071999")
   	{
   	    window.location.href="2.html";
   	}
   	else
   	{
-  		alert("your name should be wong jian hao\nyour date of birth should be 08032002");
+  		alert("your name should be chia pik ying\nyour date of birth should 12071999");
   		window.location.href="open_this_file.html";
   	}
   	
