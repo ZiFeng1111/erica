4d6ed1d951cfe7468afb805522941b335654337d
ZiFeng1111 <zfwong1111@gmail.com>
2021-06-25 15:57:24 +0800

    name_changed

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var a=prompt("Enter your Name (format : all small letters)");
+var a=prompt("Enter your Name");
 var b=prompt("Enter your date of birth (format : DDMMYYYY)");
   	if(a== "chia pik ying" && b=="12071999")
   	{
@@ -6,7 +6,7 @@ var b=prompt("Enter your date of birth (format : DDMMYYYY)");
   	}
   	else
   	{
-  		alert("your name should be chia pik ying\nyour date of birth should 12071999");
+  		alert("your name should be chia pik ying\nyour date of birth should be 12071999");
   		window.location.href="open_this_file.html";
   	}
   	
